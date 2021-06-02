4840e667c0348ca80267650c3c1e35ab92288f7c
umairahmed88 <umairahmed_88@ymail.com>
2021-06-02 15:07:15 +0500

    added image styling

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,13 +1,10 @@
 import React from 'react';
+import ImageCard from './ImageCard';
 import './imageList.css';
 
 const ImageList = (props) => {
-    const images = props.images.map(({ description, id, urls }) => {
-        return <img 
-            src={urls.regular} 
-            alt={description}
-            key={id} 
-        />
+    const images = props.images.map((image) => {
+        return <ImageCard key={image.id} image={image} />
     })
     return (
         <div className='image-list'>{images}</div>
